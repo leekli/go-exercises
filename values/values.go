1f3b0cee5fc0b38fd1209803e1e2e5442d76e485
leekli <li.kirkham@live.com>
2023-12-30 11:21:30 +0000

    exercise 2 values

---
+++ values/values.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// String concatentation
+	fmt.Println("go" + "lang")
+
+	// Numbers
+	fmt.Println("1 + 1 =", 1+1)
+	fmt.Println("7.0 / 3.0 =", 7.0/3.0)
+
+	// Booleans
+	fmt.Println(true && false) // false
+	fmt.Println(true || false) // true 
+	fmt.Println(!true) //false
+}
